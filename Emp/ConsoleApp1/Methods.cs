50331b41621bd239680b40060073a3364d84f196
jahnavichakravarthy <59594409+jahnavichakravarthy@users.noreply.github.com>
2020-04-04 12:18:15 +0530

    Add files via upload

---
+++ Emp/ConsoleApp1/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace ConsoleApp1
+{
+    public class Methods
+    {
+        public Employee FindById(string id)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeDetailsDB"].ConnectionString))
+                {
+                    return db.Query<Employee>("Select * From EMPLOYEES" +
+                    "WHERE Id = @Id", new { id }).SingleOrDefault();//return only elemnnt or defualt value if null
+                                                                    //  SELECT* FROM Customers
+                                                                    //    WHERE Country = 'Mexico';
+                }
+            }
+        
+            catch (System.Exception e)
+            {
+
+                throw;
+            }
+
+
+        }
+    }
+}
---
+++ Emp/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            Console.WriteLine("*******EMPLOYEE DETAILS***********\nPlease enter an Id:");
+            string id = Console.ReadLine();
+            methods.FindById(id);
+
+
+        }
+    }
+}
